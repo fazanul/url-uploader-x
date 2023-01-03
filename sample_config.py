7501c1a144d282e74e29f68b38da30ae327adae2
fazanul <90233267+fazanul@users.noreply.github.com>
2023-01-03 15:31:23 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,13 +2,13 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5931996092:AAGRZLQvEqtNyWsMbAv9B34szsxwGmXxmC8")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 18098654))
+    API_HASH = os.environ.get("API_HASH" 7173005963ce23d0b58b33b8a8ac7ea0)
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "988816467").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
